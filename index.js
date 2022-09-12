ae1447117496f2bc3d6656ded88dc0655110550f
Gabriel Robert <gabirobert@gmail.com>
2022-09-12 17:10:45 -0300

    Actividad modulo 2

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo')
+
+//Activad modulo 2
+const misDatos = {
+    'nombre': 'Gabriel',
+    'edad': 36,
+    'desarrollador': true,
+    'fechaNac': new Date(1986, 4, 31)
+}
+console.log(misDatos.fechaNac)
